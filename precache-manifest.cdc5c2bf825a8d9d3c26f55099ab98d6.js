4da6827120e969a063e1d1779ceb4ab84af66ec7
Liviu Cristian <liviucristian@Livius-MacBook-Air.local>
2025-04-18 15:04:12 +0300

    Updates

---
+++ precache-manifest.cdc5c2bf825a8d9d3c26f55099ab98d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0931a4e448a6b9eac92f8a7b4fd5973",
+    "url": "/parent-dashboard/index.html"
+  },
+  {
+    "revision": "b236cc65694fd47295e9",
+    "url": "/parent-dashboard/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/parent-dashboard/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/parent-dashboard/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "b236cc65694fd47295e9",
+    "url": "/parent-dashboard/static/js/main.bf9fdce2.chunk.js"
+  },
+  {
+    "revision": "73adeae59aefcf04480f",
+    "url": "/parent-dashboard/static/js/runtime~main.e1dd4e09.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/parent-dashboard/static/media/logo.06e73328.svg"
+  }
+]);
